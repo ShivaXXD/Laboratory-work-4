6843acc12cc70d039835eb877fd5d6200d2bd5ae
Ai-235-Vlasiychuk <162280583+ShivaXXD@users.noreply.github.com>
2025-11-03 22:01:33 +0200

    Update package-info.java

--- package-info.java
+++ package-info.java
@@ -1,1 +1,8 @@
+/**
+ * This package contains the main entry point and supporting classes
+ * for the Humidity Control App.
+ *
+ * <p>It demonstrates integration of Spring Boot REST API components
+ * and related logic used in the laboratory project.</p>
+ */
 package app;
